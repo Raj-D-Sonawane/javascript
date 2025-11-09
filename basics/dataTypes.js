35e5d1cd188bd4573eb3a91783a16c07ddb9eead
Raj Sonawane <rajsonawane7499@gmail.com>
2025-11-09 06:11:57 +0000

    data type

---
+++ basics/dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer varsion
+
+//alert(3 +3 ) // we are using nodeJs, not browser
+
+let name = "hitesh"
+let age = 15
+let isLoggedIn = false
+
+// number => 2 to power 53
+//bigint
+//string => ""
+//boolen => true / false
+//null => standalone value
+//undefined => 
+//symbol  => unique
+
+//object 
+
+
+console.log(typeof "hitesh") // string
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
+
