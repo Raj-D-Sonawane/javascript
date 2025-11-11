2d42a29518014e129f59c5390777c645267fe25e
Raj Sonawane <rajsonawane7499@gmail.com>
2025-11-11 02:26:02 +0000

    stack and heap

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -26,10 +26,35 @@ const myFun = function name(params) {
 
 
 // console.log( typeof bigNumber);
-console.table({
-  myFun: typeof myFun,
-  myObj: typeof myObj,
-  id: typeof id,
-  heros: typeof heros
-});
+// console.table({
+//   myFun: typeof myFun,
+//   myObj: typeof myObj,
+//   id: typeof id,
+//   heros: typeof heros
+// });
+
+let myYou = "rajSonawane@123"
+let anotherYou = myYou
+
+anotherYou ="sai@123"
+
+// console.log(myYou);
+// console.log(anotherYou);
+
+
+let user = {
+  email : "user@ybl",
+  upi : "rajababi"
+}
+
+let user2 = user
+
+user2.email = "user2.com"
+
+
+console.log(user.email);
+console.log(user2.email);
+
+
+
 
