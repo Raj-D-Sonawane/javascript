a310f98298d660d8e3bd253fa8fa43a4d21d5794
Raj Sonawane <rajsonawane7499@gmail.com>
2025-11-11 14:10:21 +0000

    strings

---
+++ basics/string.js
@@ -0,0 +1,51 @@
+// const name = "hitesh"
+// const repoCount = 50
+
+// console.log(name + repoCount + "value");
+
+// console.log(`Hello my name is ${name} and my repocont is ${repoCount}`);
+
+// const gameName = new String('r-aja-bahau')
+
+// console.log(gameName[2]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+
+// console.log(gameName.indexOf('b'));
+// const newString = gameName.substring(2,6)
+// console.log(newString);
+// const anotherString = gameName.slice(-6, 1)
+
+// console.log(anotherString);
+
+
+// const string1 =  "      hiiraja    "       
+// console.log(string1);
+// console.log(string1.trim());
+
+
+// console.log(gameName.split('-'));
+
+
+
+
+let str = "hello"
+let reversed = str.split('').reverse().join('');
+console.log(reversed);
+console.log(str);
+
+/*
+split('')-> converts string to array -> [h, e ,l, l, o]
+reverse() -> reverse the array [0, l, l, e,h]
+join() -> joins elements back into a string
+
+*/
+
+
+
+
+
+
+
