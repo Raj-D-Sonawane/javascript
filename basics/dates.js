79d66de0b94c24484a21df7e2d4d4ab0deacf7af
Raj Sonawane <rajsonawane7499@gmail.com>
2025-11-12 12:28:27 +0000

    date and time

---
+++ basics/dates.js
@@ -0,0 +1,28 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toTimeString());
+// console.log(typeof myDate);
+
+// let newDate = new Date(2023, 0, 23)
+// console.log(newDate.toDateString());
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+
+// console.log(Math.floor (Date.now()/1000));
+
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getDay());
+console.log(newDate.getMonth()+1);
+
+
+
+
