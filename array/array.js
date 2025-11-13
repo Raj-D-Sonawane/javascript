e217cd952929c669a3637d4ec9bd2adc53d90d76
Raj Sonawane <rajsonawane7499@gmail.com>
2025-11-13 09:58:26 +0000

    array

---
+++ array/array.js
@@ -0,0 +1,46 @@
+//array
+
+const arr = [1,2,3,4,5]
+// console.log(arr);
+// console.log(arr[0]);
+
+const myHeros = ["raj","yeshl"]
+
+const arr2 = new Array(0,1,2,3,4)
+// console.log(arr2[0]);
+
+
+// Array method
+// // arr2.push(5)   // value add karto
+// arr2.push(6)
+// arr2.pop()   //value delete karto
+// arr2.unshift(0) //start la add karto element
+// arr2.shift()   //remove karto first element
+
+
+// const newArr = arr2.join()
+// console.log( typeof arr2);
+// console.log(typeof newArr);
+
+//slice, splice
+console.log("A ", arr2);
+
+const myn1 = arr2.slice(1,3)
+console.log(myn1);
+console.log("D", arr2);
+
+
+const myn2 = arr2.splice(1,3)
+console.log(myn2);
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ array/array2.js
@@ -0,0 +1,18 @@
+const marvel_heros = ["thor", "Ironman", "spodderman"]
+const dc_heros = ["supman", "raja","bhaja"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros[3][2]);
+
+// const allhero = marvel_heros.concat(dc_heros)
+// console.log(allhero);
+
+
+// const all_new_heros = [...marvel_heros, ...dc_heros]
+// console.log(all_new_heros);
+
+const arr =[1,2,3 , [4,5,6],7,[2,3,4,[4,67,89,]]]
+
+const real = arr.flat(Infinity)
+// console.log(real);
+
