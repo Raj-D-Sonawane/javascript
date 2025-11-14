329c7ed6daf6ecf436710015018161e8562ef49c
Raj Sonawane <rajsonawane7499@gmail.com>
2025-11-14 07:28:40 +0000

    objects

---
+++ array/object.js
@@ -0,0 +1,47 @@
+// const tinderUser =  new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sunny"
+tinderUser.isLoggedIn = false
+
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email : "some@gmail.com",
+    fullname : {
+        userfullname : {
+            firstName : "raj",
+            lastName : "Sonawane"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstName);
+
+const obj1 ={
+    1: "a",
+    2 : "b"
+}
+const obj2 = {3 : "c", 4 : "d"}
+
+// const obj3 = {obj1,obj2}
+// const obj3 = Object.assign({},obj1,obj2)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+const user = [
+    {
+        id : 1,
+        email : "h@gmail.com"
+    }
+]
+// console.log(user[0].id)
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLogged'));
+
