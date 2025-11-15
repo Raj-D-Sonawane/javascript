49c6d2da9344e92bbf1d10dc2373a23d50d56dc6
Raj Sonawane <rajsonawane7499@gmail.com>
2025-11-15 07:15:48 +0000

    function

---
+++ functions/01_Functon.js
@@ -0,0 +1,20 @@
+// function sayMyName() {
+//     console.log("R");
+//      console.log("A");
+//       console.log("J");
+    
+// }
+
+// sayMyName() //execution
+// sayMyName   //refernse
+
+
+// function addTwoNumbers(num1, num2){  // num1,num2 parameters
+// // let result = num1 + num2
+// // return result
+//     return num1 + num2
+// }
+
+// const result = addTwoNumbers(3, 4)  // arguments
+// // console.log(result);
+
