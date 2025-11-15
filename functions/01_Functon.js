79a10cdbeba934b1008b72688c2054d2db5c9915
Raj Sonawane <rajsonawane7499@gmail.com>
2025-11-15 13:27:12 +0000

    function

--- functions/01_Functon.js
+++ functions/01_Functon.js
@@ -18,3 +18,35 @@
 // const result = addTwoNumbers(3, 4)  // arguments
 // // console.log(result);
 
+function calculateCartPrice(...num1) {
+    return num1
+    
+}
+
+// console.log(calculateCartPrice(200, 400,500, 2000));
+
+
+
+// console.log("hello");
+
+const user = {
+    username : "hitesh",
+    prices : 199
+}
+
+function handleObject(anyobj){
+    console.log(`username is ${anyobj.username} and price is ${anyobj.price } `);
+    
+}
+
+// handleObject(user)
+handleObject({
+    username : "sam",
+    price : 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+function returnSecValue (getArray){
+    return getArray[1]
+}
+console.log(returnSecValue(myNewArray));
