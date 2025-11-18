d70cb2a9ff54a62796d1b9c68243d623615f0726
Raj Sonawane <rajsonawane7499@gmail.com>
2025-11-18 09:25:09 +0000

    function

--- functions/01_Functon.js
+++ functions/01_Functon.js
@@ -1,52 +1,36 @@
-// function sayMyName() {
+// function sayHello() {
 //     console.log("R");
-//      console.log("A");
-//       console.log("J");
+//     console.log("A");
+//     console.log("J");
+    
+    
     
 // }
 
-// sayMyName() //execution
-// sayMyName   //refernse
+// sayHello()
 
 
-// function addTwoNumbers(num1, num2){  // num1,num2 parameters
-// // let result = num1 + num2
-// // return result
-//     return num1 + num2
+// function addTwoNumbers(num1, num2){
+//     console.log(num1 + num2);
+    
 // }
 
-// const result = addTwoNumbers(3, 4)  // arguments
-// // console.log(result);
-
-function calculateCartPrice(...num1) {
-    return num1
-    
+function addTwoNumbers(num1, num2){
+    // let result = num1 + num2
+    // return result;
+    return num1 + num2
 }
 
-// console.log(calculateCartPrice(200, 400,500, 2000));
-
-
-
-// console.log("hello");
-
-const user = {
-    username : "hitesh",
-    prices : 199
+ const result = addTwoNumbers(3, 4)
+//  console.log(result);
+ 
+function loginUserMassage(username = "sam") {
+    if(!username){
+        console.log("please enter a username");
+        return
+        
+    }
+    return `${username} just logged in`
 }
 
-function handleObject(anyobj){
-    console.log(`username is ${anyobj.username} and price is ${anyobj.price } `);
-    
-}
-
-// handleObject(user)
-handleObject({
-    username : "sam",
-    price : 399
-})
-
-const myNewArray = [200, 400, 100, 600]
-function returnSecValue (getArray){
-    return getArray[1]
-}
-console.log(returnSecValue(myNewArray));
+console.log(loginUserMassage("raj"))
