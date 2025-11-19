d6241e4bc549d5ca7a7f5b1d2ecffac053739655
Raj Sonawane <rajsonawane7499@gmail.com>
2025-11-19 08:41:04 +0000

    switch

---
+++ controlFlow/one.js
@@ -0,0 +1,57 @@
+// if 
+
+const isUserloggedIn = true
+const temparature = 41
+
+// if (temparature === 41) {
+//     console.log("less than 50");
+
+//     }
+//     else{
+//         console.log("more than 50");
+        
+//     }
+//<,>,<=,==,!=, === , !==
+
+// const score = 200
+
+// if(score > 100) {
+//     let power = "fly"
+//     console.log(`user power : ${power}`);
+    
+// }
+
+// const balance = 1000
+
+// // if(balance > 500) console.log(("test"));
+
+// if (balance < 500) {
+//     console.log("less than");
+    
+
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if ( balance < 900) {
+//     console.log("less than 900");
+    
+// } else {
+//     console.log("less than 1200");
+    
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if ( userLoggedIn && debitCard)
+{
+    console.log("Allow to buy course");
+    
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("user logged in");
+    
+}
