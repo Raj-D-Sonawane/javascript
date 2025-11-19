cbf1d9f027a150fe114b8d26930dc3da0283e65b
Raj Sonawane <rajsonawane7499@gmail.com>
2025-11-19 16:54:38 +0000

    for of

---
+++ iterations/three.js
@@ -0,0 +1,55 @@
+// // for of
+
+// const arr = [1,2,3,4,5]
+
+// for (const i of arr) {
+//     console.log(i);
+    
+    
+// }
+
+// const str = "hello world"
+// for( const string of str) console.log(string);
+
+// //Maps
+
+// const map = new Map()
+// map.set ('IN' ,'INDIA')
+// map.set ('USA', "United States of America")
+// map.set ('Fr', "France")
+
+// console.log(map);
+
+// for (const [element,value] of map) {
+//     console.log(element , value);
+    
+    
+// }
+//  const obj = {
+//     'game1' : 'NFS',
+//     'game2' : 'Spider'
+//  }
+
+// for (const key in obj) {
+  
+//     console.log(obj[key]);
+    
+    
+// }
+
+// const p = ["js","rb","py"]
+
+// for (const key in p) {
+//     console.log(p[key]);
+    
+    
+    
+// }
+
+// const c = ["js", "ruby", "c"]
+// c.forEach(element => console.log(element));
+
+// function printMe(item) {
+//     console.log(item);
+    
+// }
