7ba4c3c16103dccdbfc2195f76b8a3501d15248d
Raj Sonawane <rajsonawane7499@gmail.com>
2025-11-19 16:13:10 +0000

    do while

---
+++ iterations/two.js
@@ -0,0 +1,23 @@
+let i = 0
+while (i <= 10) {
+    console.log(i);
+    i++
+    
+    
+}
+
+let myArr = ["raj", "yash", "dipak"]
+let j = 0;
+
+while (j < myArr.length) {
+    console.log(myArr[j]);
+    j++
+    
+    
+}
+let sc = 11
+do {
+    console.log(sc);
+    sc++
+    
+} while (sc <= 10);
