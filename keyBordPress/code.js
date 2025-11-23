8bea6d75521f680de4ecc6d00e97c6582227dbab
Raj Sonawane <rajsonawane7499@gmail.com>
2025-11-23 07:14:37 +0000

    key bord press

---
+++ keyBordPress/code.js
@@ -0,0 +1,19 @@
+const insert = document.getElementById('insert')
+
+window.addEventListener('keydown', (e) => {
+    insert.innerHTML = `<div >
+    <table>
+    <tr>
+    <td>key</td>
+    <td>keycode</td>
+    <td>code</td>
+    </tr>
+     <tr>
+    <td>${e.key === " " ? "Space" : e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+    </tr>
+    </table>
+    
+    </div>`
+})
