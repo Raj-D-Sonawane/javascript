311576a304cd07110169e2ab49b8ad2fcb97eadc
Raj Sonawane <rajsonawane7499@gmail.com>
2025-11-26 12:45:09 +0000

    Find duplicates in an array

--- array/second.js
+++ array/second.js
@@ -6,3 +6,21 @@
 
 //++++++++++++++++++++++++++++++++++++++++++
 
+//Find duplicates in an array.
+let arr = [1, 2, 3,  4, 3, 5];
+
+let seen = new Set();
+let duplicates = new Set();
+
+ for(let num of arr) {
+    if (seen.has(num)) {
+        duplicates.add(num);
+    }
+
+    else {
+        seen.add(num);
+    }
+ }
+
+ console.log([...duplicates]);
+ 
